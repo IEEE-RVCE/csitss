c0a87ebffe39a8362271a3927fd361f329304de0
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2022-10-18 01:42:00 +0530

    added footer-icons

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -142,7 +142,64 @@ const Footer = () => {
               Collaboration with{" "}
             </span>
             <Box>
-              <HeaderImages />           
+              <div className="ml-10 flex flex-nowrap md:ml-20 lg:ml-40 justify-evenly">
+                <div className="inline-block px-3">
+                  <div className="m-5 ml-0 h-auto w-auto max-w-xs overflow-hidden rounded-lg bg-none p-2 transition-shadow duration-300 ease-in-out hover:shadow-xl">
+                    <a
+                      href="https://www.rvce.edu.in/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <img
+                        className="h-full w-full rounded-full object-cover"
+                        src="RVCE NEW-HEADER.png"
+                        alt="rvce_logo"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="inline-block px-3">
+                  <div className="w-auto h-auto max-w-xs overflow-hidden rounded-lg bg-none m-5 p-2 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                    <a href="https://www.ieee.org" target="_blank" rel="noreferrer">
+                      <img
+                        className="w-auto h-auto object-cover"
+                        src="logo_IEEE_Main.png"
+                        alt="rvce_logo"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="inline-block px-3">
+                  <div className="w-auto h-auto max-w-xs overflow-hidden rounded-lg  bg-none m-5 pt-6 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                    <a
+                      href="https://ieeebangalore.org/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <img
+                        className="w-auto h-auto object-cover"
+                        src="logo_IEEE_Banaglaore_Section.png"
+                        alt="rvce_logo"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="inline-block px-3">
+                  <div className="m-5 h-auto w-auto max-w-xs overflow-hidden  rounded-lg bg-none pt-11 transition-shadow duration-300 ease-in-out hover:shadow-xl">
+                    <a
+                      href="https://ieee-rvce.org/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <img
+                        className="h-auto w-auto object-cover"
+                        src="logo_rvce_ieee.png"
+                        alt="rvce_logo"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
             </Box>
 
           </div>
