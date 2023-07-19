f0aca7cd283920e3a7c24a60a71ad04825339276
Mayur S Chittaragi <mayurschittaragi@gmail.com>
2023-07-19 08:54:26 +0530

    updated brochure

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,7 +75,7 @@ export default function Index() {
                     TimeLine
                   </ScrollLink>
                   <a
-                    href="CSITSS_27-06-2023.pdf"
+                    href="CSITSS_19-07-2023.pdf"
                     className="mt-4 rounded bg-blue-600 px-4 py-2 text-sm font-medium uppercase text-white hover:bg-blue-500 focus:bg-blue-500 focus:outline-none"
                   >
                     Download Brochure
