1a7f7c62e7d9ff0df41b0731bcca9d054a6b2439
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2023-10-08 22:54:40 +0530

    added styling to contact us page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,100 +1,87 @@
 import React from "react";
-
+import { PhoneIcon } from "@heroicons/react/solid";
 function contact() {
   return (
     <>
-      <div className="flex flex-col sm:flex-row">
-        <div className="relative z-10 w-full md:w-1/2 lg:w-1/2">
-          <table className="mx-auto mt-40 border-collapse border border-gray-300">
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Name</th>
-              <th className="border border-gray-300 px-4 py-2">
-                Contact Number
-              </th>
-            </tr>
-            <tr>
-              <td className="border border-gray-300 px-4 py-2">
-                Dr. Abhilash Krishna D G
-              </td>
-              <td className="border border-gray-300 px-4 py-2">
-                +91 7989341864
-              </td>
-            </tr>
-            <tr>
-              <td className="border border-gray-300 px-4 py-2">
-                Dr. Parth Sarathi Panigrahy
-              </td>
-              <td className="border border-gray-300 px-4 py-2">
-                +91 8847834371
-              </td>
-            </tr>
-            <tr>
-              <td className="border border-gray-300 px-4 py-2">
-                Dr. Pandry Narendra Rao
-              </td>
-              <td className="border border-gray-300 px-4 py-2">
-                +91 9885414398
-              </td>
-            </tr>
-            <tr>
-              <td className="border border-gray-300 px-4 py-2">
-                Dr. Vandana Jha{" "}
-              </td>
-              <td className="border border-gray-300 px-4 py-2">
-                +91 8969977560
-              </td>
-            </tr>
-          </table>
-        </div>
-        <div className="relative z-10 mt-10 flex w-full flex-col rounded-lg p-8 shadow-md md:w-1/2 lg:w-1/2">
-          {/*   <h2 className="title-font mb-1 text-lg font-medium text-white">
-            Contact Us
-          </h2>
-          <p className="mb-5 leading-relaxed text-white">
-            Any Doubts pertaining to the CSITSS
-          </p>
-          <form action="https://formspree.io/f/xrgrdozk" method="POST">
-            <div className="relative mb-4">
-              <label
-                htmlFor="email"
-                className="text-sm leading-7 text-gray-400"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full rounded border border-gray-700 bg-gray-800 py-1 px-3 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-              />
+      <div className="container my-4 mx-auto bg-gray-100 p-4">
+        <div className="grid grid-cols-1 gap-1 md:grid-cols-2">
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1485770958101-9dd7e4ea6d93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80"
+              alt="contact"
+              className="h-auto w-full rounded-xl"
+            />
+          </div>
+          <div>
+            <div className="md:flex md:flex-col md:justify-between">
+              <div className="mb-4 p-2 md:mb-0 ">
+                <table className="mx-auto w-full border-collapse border border-gray-300 text-sm">
+                  <tr>
+                    <th className="border border-black px-4 py-2 text-left">
+                      Name
+                    </th>
+                    <th className="border border-black px-4 py-2 text-left">
+                      Contact Number
+                    </th>
+                  </tr>
+                  <tr>
+                    <td className="border border-black px-4 py-2">
+                      Dr. Abhilash Krishna D G
+                    </td>
+
+                    <td className="flex-row border border-black px-4 py-2">
+                      <div className="flex">
+                        <PhoneIcon className="mx-2 h-4 w-4" />
+                        +91 7989341864
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-black px-4 py-2">
+                      Dr. Parth Sarathi Panigrahy
+                    </td>
+                    <td className="border border-black px-4 py-2">
+                      <div className="flex">
+                        <PhoneIcon className="mx-2 h-4 w-4" />
+                        +91 8847834371
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-black px-4 py-2">
+                      Dr. Pandry Narendra Rao
+                    </td>
+                    <td className="border border-black px-4 py-2">
+                      <div className="flex">
+                        <PhoneIcon className="mx-2 h-4 w-4" />
+                        +91 9885414398
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-black px-4 py-2">
+                      Dr. Vandana Jha{" "}
+                    </td>
+                    <td className="border border-black px-4 py-2">
+                      <div className="flex">
+                        <PhoneIcon className="mx-2 h-4 w-4" />
+                        +91 8969977560
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <div className="mt-4">
+                <h3 className="h-full text-center align-middle text-sm md:text-lg">
+                  For any queires, please mail us at
+                  <br />
+                  <a href="mailto:csitss@rvce.edu.in">
+                    <span className="font-bold">csitss@rvce.edu.in</span>
+                  </a>
+                </h3>
+              </div>
             </div>
-            <div className="relative mb-4">
-              <label
-                htmlFor="message"
-                className="text-sm leading-7 text-gray-400"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="h-32 w-full resize-none rounded border border-gray-700 bg-gray-800 py-1 px-3 text-base leading-6 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900"
-              ></textarea>
-            </div>
-            <button className="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none">
-              Send
-            </button>
-          </form>
-          <p className="mt-3 text-xs text-gray-400 text-opacity-90">
-            Your email will be not shared with anyone.
-          </p> */}
-          <h3 className="h-full pt-32 text-center align-middle">
-            For any queires, please mail us at
-            <br />
-            <a href="mailto:csitss@rvce.edu.in">
-              <span className="font-bold">csitss@rvce.edu.in</span>
-            </a>
-          </h3>
+          </div>
         </div>
       </div>
     </>
