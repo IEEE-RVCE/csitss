4b9a3a5a8efbea224b7de211b9ea9a1cdde56a78
ayushratan082 <ayushratan082@gmail.com>
2023-10-12 14:30:02 +0530

    Sposors

--- pages/sponsorship.tsx
+++ pages/sponsorship.tsx
@@ -20,10 +20,74 @@ const SponsorsData: Sponsor[] = [
     type: "others",
     description: "xxx",
   },
+  {
+    name: "ARCTICERN",
+    // all images should be in public/sponsors folder, just put the name of the image here and it will be loaded
+    logo: "ARCTICTERN.png",
+    link: "https://www.ARCTICTERN.com/",
+    type: "others",
+    description: "xxx",
+
+  },
+  {
+    name: "Apraava Energy",
+    // all images should be in public/sponsors folder, just put the name of the image here and it will be loaded
+    logo: "Apraava Energy.png",
+    link: "https://www.apraava.com//",
+    type: "others",
+    description: "xxx",
+
+  },
+  {
+    name: "LAPP Kabel",
+    // all images should be in public/sponsors folder, just put the name of the image here and it will be loaded
+    logo: "LAPP Kabel.png",
+    link: "https://www.lapp.com/en/fr/",
+    type: "others",
+    description: "xxx",
+
+  },
+  {
+    name: "Log9",
+    // all images should be in public/sponsors folder, just put the name of the image here and it will be loaded
+    logo: "Log9.png",
+    link: "https://www.log9materials.com/",
+    type: "others",
+    description: "xxx",
+
+  },
+  {
+    name: "Rexroth",
+    // all images should be in public/sponsors folder, just put the name of the image here and it will be loaded
+    logo: "Rexroth.jpg",
+    link: "https://www.boschrexroth.com/en/dc/",
+    type: "others",
+    description: "xxx",
+
+  },
+  {
+    name: "Synaptics",
+    // all images should be in public/sponsors folder, just put the name of the image here and it will be loaded
+    logo: "Synaptics.png",
+    link: "https://www.synaptics.com//",
+    type: "others",
+    description: "xxx",
+
+  },
+  {
+    name: "Vena Energy",
+    // all images should be in public/sponsors folder, just put the name of the image here and it will be loaded
+    logo: "Vena Energy.png",
+    link: "https://www.venaenergy.com/",
+    type: "others",
+    description: "xxx",
+
+  }
   // Add more sponsors as needed
 ];
 
 // don't worry about this, this just loops through the sponsors and groups them by type helps with easier rendering
+
 const Sponsorship: React.FC = () => {
   const groupedSponsors: Record<SponsorType, Sponsor[]> = {
     diamond: [],
@@ -32,10 +96,13 @@ const Sponsorship: React.FC = () => {
     silver: [],
     others: [],
   };
+  
 
   SponsorsData.forEach((sponsor) => {
     groupedSponsors[sponsor.type].push(sponsor);
   });
+
+  
 
   const sponsorGroups = Object.entries(groupedSponsors).map(
     ([type, sponsors]) => {
@@ -44,15 +111,25 @@ const Sponsorship: React.FC = () => {
       }
 
       const items = sponsors.map((item) => (
-        <div key={item.name} className="mb-4 rounded-lg bg-white p-4 shadow-lg">
-          <ThemeIcon variant="light" size={100} radius="md">
-            <Image src={`/sponsors/${item.logo}`} alt={item.name} />
-          </ThemeIcon>
-          <div className="mt-4">
-            <Text size="lg" className="font-bold text-blue-500">
-              {item.name}
-            </Text>
-            <Text className="text-gray-700">{item.description ?? ""}</Text>
+        <div
+          key={item.name}
+          className="mb-4 rounded-lg bg-white p-4 shadow-lg"
+          style={{ border: "2px solid black" }} // Add this line to add the border
+        >
+          <div className="flex items-center">
+            <div className="mr-4">
+              <ThemeIcon variant="light" size={100} radius="md">
+                <Image src={`/sponsors/${item.logo}`} alt={item.name} />
+              </ThemeIcon>
+            </div>
+            <div style={{ flex: 1 }}>
+              <div>
+                <Text size="lg" className="font-bold text-blue-500">
+                  {item.name}
+                </Text>
+                <Text className="text-gray-700">{item.description ?? ""}</Text>
+              </div>
+            </div>
           </div>
         </div>
       ));
@@ -69,6 +146,8 @@ const Sponsorship: React.FC = () => {
       );
     }
   );
+
+
 
   return (
     <div className="bg-gray-100 p-4">
