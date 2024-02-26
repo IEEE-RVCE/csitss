e1f6d10437315ec75cd3e0655306332a89dbc796
Tejas Pattar <tejaspattar03@gmail.com>
2024-02-26 16:16:59 +0530

    brochure changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,7 +72,7 @@ export default function Index() {
                     TimeLine
                   </ScrollLink>
                   <a
-                    href="Brochure - V3.pdf"
+                    href="Brochure - V3.2 (1).pdf"
                     className="mt-4 rounded bg-blue-600 px-4 py-2 text-sm font-medium uppercase text-white hover:bg-blue-500 focus:bg-blue-500 focus:outline-none"
                   >
                     Download Brochure
