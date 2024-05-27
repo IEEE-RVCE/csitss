7dc77ccb6e3528679ce6810225140fb4bf0cba76
khushal <khushal2004a@gmail.com>
2024-05-27 16:01:54 +0530

    built the code

--- pages/committee1.tsx
+++ pages/committee1.tsx
@@ -5,21 +5,21 @@ export default function committee1() {
         <div className="grid grid-cols-1 lg:grid-cols-2 w-2/3 place-items-center">
             <div className="md:w-2/3 w-4/5 ">
                 <Card committeeName={"Conference Chairs"} to={"/conference_chairs"}/>
-                <Card committeeName={"Program Chairs"}/>
-                <Card committeeName={"Technical Program Chairs"}/>
-                <Card committeeName={"Finance Chairs"}/>
-                <Card committeeName={"Publication Chairs"}/>
-                <Card committeeName={"Sponsorship Chairs"}/>
-                <Card committeeName={"Publicity and Media Chairs"}/>
+                <Card committeeName={"Program Chairs"} to={""}/>
+                <Card committeeName={"Technical Program Chairs"} to={""}/>
+                <Card committeeName={"Finance Chairs"} to={""}/>
+                <Card committeeName={"Publication Chairs"} to={""}/>
+                <Card committeeName={"Sponsorship Chairs"} to={""}/>
+                <Card committeeName={"Publicity and Media Chairs"} to={""}/>
             </div>
             <div className="md:w-2/3 w-4/5">
-                <Card committeeName={"Website Chairs"}/>
-                <Card committeeName={"Conference Convenors"}/>
-                <Card committeeName={"Session Management"}/>
-                <Card committeeName={"Hospitality"}/>
-                <Card committeeName={"Transportation"}/>
-                <Card committeeName={"Stage Committee"}/>
-                <Card committeeName={"Registration Committee"}/>
+                <Card committeeName={"Website Chairs"} to={""}/>
+                <Card committeeName={"Conference Convenors"} to={""}/>
+                <Card committeeName={"Session Management"} to={""}/>
+                <Card committeeName={"Hospitality"} to={""}/>
+                <Card committeeName={"Transportation"} to={""}/>
+                <Card committeeName={"Stage Committee"} to={""}/>
+                <Card committeeName={"Registration Committee"} to={""}/>
             </div>
         </div>
     </div>
