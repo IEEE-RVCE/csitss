af06e0740650880e9f1b80246f78dee00b498bd5
khushal <khushal2004a@gmail.com>
2024-05-27 15:35:25 +0530

    changes in responsiveness

--- pages/committee1.tsx
+++ pages/committee1.tsx
@@ -1,9 +1,9 @@
 import Card from "../components/Card";
 
-export default function card() {
+export default function committee1() {
     return <div className="flex justify-center items-center w-screen">
-        <div className="grid grid-cols-2 w-2/3 place-items-center">
-            <div className="w-2/3">
+        <div className="grid grid-cols-1 lg:grid-cols-2 w-2/3 place-items-center">
+            <div className="md:w-2/3 w-4/5 ">
                 <Card committeeName={"Conference Chairs"} to={"/conference_chairs"}/>
                 <Card committeeName={"Program Chairs"}/>
                 <Card committeeName={"Technical Program Chairs"}/>
@@ -12,7 +12,7 @@ export default function card() {
                 <Card committeeName={"Sponsorship Chairs"}/>
                 <Card committeeName={"Publicity and Media Chairs"}/>
             </div>
-            <div className="w-2/3">
+            <div className="md:w-2/3 w-4/5">
                 <Card committeeName={"Website Chairs"}/>
                 <Card committeeName={"Conference Convenors"}/>
                 <Card committeeName={"Session Management"}/>
