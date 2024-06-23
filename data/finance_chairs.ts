f01248589c78653319d87ed2018d70ba41b824f3
khushal <khushal2004a@gmail.com>
2024-06-23 10:33:04 +0530

    added data for committee_data

---
+++ data/finance_chairs.ts
@@ -0,0 +1,7 @@
+export const finance_chairs_data = [
+    {
+      name: 'Ravi Varma RB',
+      post: 'System Administrator',
+      imgLink:'https://media.licdn.com/dms/image/C5603AQHqhq9mk15kXA/profile-displayphoto-shrink_400_400/0/1639220175636?e=1714608000&v=beta&t=EOQOliI-69KnRETUyUGQFySvpRmkz9iOhGgQFZzmNRg',
+    },
+]
