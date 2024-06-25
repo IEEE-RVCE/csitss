916fdf908bcdd02bdd9e0d7bb93b98f7be8febe1
Sundarakrishnan N <72200790+SundarakrishnanN@users.noreply.github.com>
2024-06-25 21:56:38 +0530

    updated four chairs (#71)
    * updated four chairs
    
    
    ---------
    
    Co-authored-by: chethu3 <chethohaarjm@gmail.com>

--- pages/committee1.tsx
+++ pages/committee1.tsx
@@ -7,9 +7,9 @@ export default function committee1() {
                 <Card committeeName={"Conference Chairs"} to={"/committee-details/conference_chairs"}/>
                 <Card committeeName={"Program Chairs"} to={"/committee-details/program_chairs"}/>
                 <Card committeeName={"Technical Program Chairs"} to={""}/>
-                <Card committeeName={"Finance Chairs"} to={""}/>
-                <Card committeeName={"Publication Chairs"} to={""}/>
-                <Card committeeName={"Sponsorship Chairs"} to={""}/>
+                <Card committeeName={"Finance Chairs"} to={"/committee-details/finance_chairs"}/>
+                <Card committeeName={"Publication Chairs"} to={"/committee-details/publication_chairs"}/>
+                <Card committeeName={"Sponsorship Chairs"} to={"/committee-details/sponsorship_chairs"}/>
                 <Card committeeName={"Publicity and Media Chairs"} to={"/committee-details/pm_chairs"}/>
             </div>
             <div className="md:w-2/3 w-4/5">
