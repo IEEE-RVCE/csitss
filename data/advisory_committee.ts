9542da57cd634ebea6bfd46301d702bfd927b444
SundarakrishnanN <sundar050804@gmail.com>
2024-06-26 21:25:55 +0530

    added new committees

---
+++ data/advisory_committee.ts
@@ -0,0 +1,21 @@
+const advisory_committee = [
+    {
+        name: 'Sri. SS Iyengar',
+        post: 'Professor, Ryder Professor of Computer Science, Florida International University (FIU), Miami',
+        imgLink:'https://www.cis.fiu.edu/wp-content/uploads/2016/07/S-S-Iyengar.jpg'
+  
+      },
+      {
+          name: 'Prof. Dr.-Ing. Vinod Rajamani',
+          post: 'Vice Dean, Mechanical Engineering, Fachhochscule Dortmund University',
+          imgLink:'https://media.licdn.com/dms/image/C4D03AQGjhM5qS_TDQA/profile-displayphoto-shrink_400_400/0/1517247854328?e=1724889600&v=beta&t=dxpdpNBwZda69csnAYQuyycnmDYgJScZnFTva1m6MDI'
+    
+        },
+        {
+          name: 'Dr. Kavi Narayana Murthy',
+          post: ', Professor, Department of Computer and Information Sciences, University of Hyderabad, Hyderabad',
+          imgLink:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyfyvUvmvzvPz3twNytT0jHgk2FLa18FUUgw&s'
+    
+        },
+]
+export default advisory_committee;
