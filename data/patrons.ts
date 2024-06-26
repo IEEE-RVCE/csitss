4e794878b4e101a32ec2edfe22bd7583ad4eafd0
Sundarakrishnan N <72200790+SundarakrishnanN@users.noreply.github.com>
2024-06-26 23:14:01 +0530

    added new committees (#74)
    * added new committees
    
    * add remaining advisory committee
    
    ---------
    
    Co-authored-by: Chirag <chiragwork2403@gmail.com>

---
+++ data/patrons.ts
@@ -0,0 +1,22 @@
+const patrons = [
+    {
+      name: 'Sri. K G Subbarama Setty',
+      post: 'Hon. Treasurer, RSST',
+      imgLink:'https://www.rvschool.edu.in/wp-content/uploads/2024/01/Untitled-1.png'
+
+    },
+    {
+        name: 'Dr. AVS Murthy',
+        post: 'Hon. Secretary, RSST',
+        imgLink:'https://rvu.edu.in/wp-content/uploads/2023/02/02-3.png'
+  
+      },
+      {
+        name: 'Sri. D P Nagaraj',
+        post: 'Hon. Joint Secretary, RSST',
+        imgLink:'https://rvu.edu.in/wp-content/uploads/2023/02/01-3.png'
+  
+      },
+      
+]
+export default patrons;
--- pages/committee1.tsx
+++ pages/committee1.tsx
@@ -4,6 +4,7 @@ export default function committee1() {
     return <div className="flex justify-center items-center w-screen">
         <div className="grid grid-cols-1 lg:grid-cols-2 w-2/3 place-items-center">
             <div className="md:w-2/3 w-4/5 ">
+                <Card committeeName={"Patrons"} to={"/committee-details/patrons"}/>
                 <Card committeeName={"Conference Chairs"} to={"/committee-details/conference_chairs"}/>
                 <Card committeeName={"Program Chairs"} to={"/committee-details/program_chairs"}/>
                 <Card committeeName={"Technical Program Chairs"} to={"/committee-details/technical_program_chairs"}/>
@@ -13,6 +14,7 @@ export default function committee1() {
                 <Card committeeName={"Publicity and Media Chairs"} to={"/committee-details/pm_chairs"}/>
             </div>
             <div className="md:w-2/3 w-4/5">
+                <Card committeeName={"Advisory Committee"} to={"/committee-details/advisory_committee"}/>
                 <Card committeeName={"Website Chairs"} to={"/committee-details/website_chairs"}/>
                 <Card committeeName={"Conference Convenors"} to={"/committee-details/conference_convenors"}/>
                 <Card committeeName={"Session Management"} to={"/committee-details/session_management"}/>
