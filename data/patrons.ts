c576bd411a6f30b6e3fa2ff31fd7edc4ca1a2572
Sundarakrishnan N <72200790+SundarakrishnanN@users.noreply.github.com>
2024-06-27 23:28:03 +0530

    cmt update

--- data/patrons.ts
+++ data/patrons.ts
@@ -1,22 +1,23 @@
 const patrons = [
-    {
-      name: 'Sri. K G Subbarama Setty',
-      post: 'Hon. Treasurer, RSST',
-      imgLink:'https://www.rvschool.edu.in/wp-content/uploads/2024/01/Untitled-1.png'
+
+  {
+      name: 'Dr. AVS Murthy',
+      post: 'Hon. Secretary, RSST',
+      imgLink:'https://rvu.edu.in/wp-content/uploads/2023/02/02-3.png'
 
     },
     {
-        name: 'Dr. AVS Murthy',
-        post: 'Hon. Secretary, RSST',
-        imgLink:'https://rvu.edu.in/wp-content/uploads/2023/02/02-3.png'
-  
-      },
-      {
-        name: 'Sri. D P Nagaraj',
-        post: 'Hon. Joint Secretary, RSST',
-        imgLink:'https://rvu.edu.in/wp-content/uploads/2023/02/01-3.png'
-  
-      },
-      
+      name: 'Sri. D P Nagaraj',
+      post: 'Hon. Joint Secretary, RSST',
+      imgLink:'https://rvu.edu.in/wp-content/uploads/2023/02/01-3.png'
+
+    },
+    {
+      name: 'Sri. Venkatesh Babu',
+      post: 'Hon. Treasurer, RSST',
+      imgLink:'https://nmkrv.edu.in/wp-content/uploads/2023/06/BOM-pics-1.png'
+
+    },
+    
 ]
 export default patrons;
