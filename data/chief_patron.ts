eb89b8053b708b017569f496908c49ae03301829
SundarakrishnanN <sundar050804@gmail.com>
2024-06-27 11:12:56 +0530

    added chieft patron

---
+++ data/chief_patron.ts
@@ -0,0 +1,10 @@
+const chief_patron=
+[
+    {
+        name: 'Dr. MP Shyamm',
+        post: 'President, RSST',
+        imgLink:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJWx98LkAMrfC1ZtWNWZ_pM0iyEik9a2NNZh4PUJYaEdAHdzlTMmu8&usqp=CAE&s',
+      }
+]
+export default chief_patron;
--- pages/committee.tsx
+++ pages/committee.tsx
@@ -1,28 +1,30 @@
 import Card from "../components/Card";
 
-export default function committee1() {
-    return <div className="flex justify-center items-center w-screen">
-        <div className="grid grid-cols-1 lg:grid-cols-2 w-2/3 place-items-center">
-            <div className="md:w-2/3 w-4/5 ">
-                <Card committeeName={"Patrons"} to={"/committee-details/patrons"}/>
-                <Card committeeName={"Conference Chairs"} to={"/committee-details/conference_chairs"}/>
-                <Card committeeName={"Program Chairs"} to={"/committee-details/program_chairs"}/>
-                <Card committeeName={"Technical Program Chairs"} to={"/committee-details/technical_program_chairs"}/>
-                <Card committeeName={"Finance Chairs"} to={"/committee-details/finance_chairs"}/>
-                <Card committeeName={"Publication Chairs"} to={"/committee-details/publication_chairs"}/>
-                <Card committeeName={"Sponsorship Chairs"} to={"/committee-details/sponsorship_chairs"}/>
-                <Card committeeName={"Publicity and Media Chairs"} to={"/committee-details/pm_chairs"}/>
-            </div>
-            <div className="md:w-2/3 w-4/5">
-                <Card committeeName={"Advisory Committee"} to={"/committee-details/advisory_committee"}/>
-                <Card committeeName={"Website Chairs"} to={"/committee-details/website_chairs"}/>
-                <Card committeeName={"Conference Convenors"} to={"/committee-details/conference_convenors"}/>
-                <Card committeeName={"Session Management"} to={"/committee-details/session_management"}/>
-                <Card committeeName={"Hospitality"} to={"/committee-details/hospitality"}/>
-                <Card committeeName={"Transportation"} to={"/committee-details/transportation"}/>
-                <Card committeeName={"Stage Committee"} to={""}/>
-                <Card committeeName={"Registration Committee"} to={"/committee-details/registration_committee"}/>
+export default function Committee1() {
+    return (
+        <div className="flex flex-col justify-center items-center w-screen">
+            <div className="grid grid-cols-1 lg:grid-cols-2 w-2/3 place-items-center">
+                <div className="md:w-2/3 w-4/5 ">
+                    <Card committeeName={"Chief Patron"} to={"/committee-details/chief_patron"} />
+                    <Card committeeName={"Conference Chairs"} to={"/committee-details/conference_chairs"} />
+                    <Card committeeName={"Program Chairs"} to={"/committee-details/program_chairs"} />
+                    <Card committeeName={"Technical Program Chairs"} to={"/committee-details/technical_program_chairs"} />
+                    <Card committeeName={"Finance Chairs"} to={"/committee-details/finance_chairs"} />
+                    <Card committeeName={"Publication Chairs"} to={"/committee-details/publication_chairs"} />
+                    <Card committeeName={"Sponsorship Chairs"} to={"/committee-details/sponsorship_chairs"} />
+                    <Card committeeName={"Publicity and Media Chairs"} to={"/committee-details/pm_chairs"} />
+                </div>
+                <div className="md:w-2/3 w-4/5">
+                <Card committeeName={"Patrons"} to={"/committee-details/patrons"} />
+                    <Card committeeName={"Advisory Committee"} to={"/committee-details/advisory_committee"} />
+                    <Card committeeName={"Website Chairs"} to={"/committee-details/website_chairs"} />
+                    <Card committeeName={"Conference Convenors"} to={"/committee-details/conference_convenors"} />
+                    <Card committeeName={"Session Management"} to={"/committee-details/session_management"} />
+                    <Card committeeName={"Hospitality"} to={"/committee-details/hospitality"} />
+                    <Card committeeName={"Transportation"} to={"/committee-details/transportation"} />
+                    <Card committeeName={"Registration Committee"} to={"/committee-details/registration_committee"} />
+                </div>
             </div>
         </div>
-    </div>
+    );
 }
