f012caba6d80155e9a32e8b141ce27d57b86517a
Ayush <ayushratan082@gmail.com>
2024-07-03 17:00:58 +0530

    table updates

--- pages/committee.tsx
+++ pages/committee.tsx
@@ -2,12 +2,12 @@ import Card from "../components/Card";
 
 export default function Committee1() {
     return (
-        <div className="flex flex-col justify-center items-center w-screen">
+        <div className="flex flex-col justify-center items-center w-full overflow-hidden">
             <div className="w-full flex justify-center my-4">
                 <Card committeeName={"Chief Patron"} to={"/committee-details/chief_patron"} />
             </div>
-            <div className="grid grid-cols-1 lg:grid-cols-2 w-2/3 place-items-center">
-                <div className="md:w-2/3 w-4/5 ">
+            <div className="grid grid-cols-1 lg:grid-cols-2 w-full md:w-2/3 place-items-center gap-4 px-4">
+                <div className="w-full md:w-2/3 max-w-full">
                     <Card committeeName={"Patrons"} to={"/committee-details/patrons"} />
                     <Card committeeName={"Conference Chairs"} to={"/committee-details/conference_chairs"} />
                     <Card committeeName={"Program Chairs"} to={"/committee-details/program_chairs"} />
@@ -17,7 +17,7 @@ export default function Committee1() {
                     <Card committeeName={"Sponsorship Chairs"} to={"/committee-details/sponsorship_chairs"} />
                     <Card committeeName={"Publicity and Media Chairs"} to={"/committee-details/pm_chairs"} />
                 </div>
-                <div className="md:w-2/3 w-4/5">
+                <div className="w-full md:w-2/3 max-w-full">
                     <Card committeeName={"Organizing Committee"} to={"/committee-details/organizing_committee"} />
                     <Card committeeName={"Advisory Committee"} to={"/committee-details/advisory_committee"} />
                     <Card committeeName={"Website Chairs"} to={"/committee-details/website_chairs"} />
