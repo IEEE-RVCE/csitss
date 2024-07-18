782368c10f79b616148e49f52e9231522bc2d003
VaibhavPRoddappanavar <vaibhavpr381@gmail.com>
2024-07-18 17:26:49 +0530

    Fixed the footer issues regarding the phoneview.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
-import { AppShell, Header, Image, Menu } from '@mantine/core'
+import { AppShell, Center, Header, Image, Menu } from '@mantine/core'
 
 import Link from 'next/link'
-import React from 'react'
 const Links: Array<{
   href: string
   label: string
@@ -214,27 +213,32 @@ const NavBar = () => {
 
 const Footer = () => {
   return (
-    <footer className="static bottom-0 w-[100%] bg-gray-50">
-      <div className="mx-auto flex max-w-screen-xl flex-col items-center px-4 py-16 sm:px-6 lg:block lg:px-8">
+    <footer className="static bottom-0 w-[100%] bg-gray-50" >
+      <div className="mx-auto flex max-w-screen-xl flex-col items-center px-4 py-16 sm:px-6 lg:block lg:px-8 mainDiv " style={{padding:'10px'}}>
+        
         <a className="flex items-center justify-around" href="/">
           <Image
             src="logo_rvce.jpg"
             alt="logo"
             className="m-5 h-full w-20"
-          />
+          /></a>
+          <a >
+            <Center>
           <Image
             src="https://i.ibb.co/dBDxKQt/GO-CHANGE-THE-WORLD.png"
             alt="GO-CHANGE-THE-WORLD"
             className="m-5 h-full w-48"
           />
+          </Center>
         </a>
         <div className="flex flex-col items-center justify-center">
           <h1 className="font-extrabold uppercase text-blue-800">
             RV College of Engineering
           </h1>
-          <h2>RV Vidyanikethan Post, Mysuru Road Bengaluru - 560059</h2>
+          <h2 style={{textAlign:'center'}}>RV Vidyanikethan Post, Mysuru Road Bengaluru - 560059</h2>
         </div>
-        <div className="mt-8 border-t border-gray-100 pt-8 sm:flex sm:items-center sm:justify-between">
+        <div className="mt-8 border-t border-gray-100 pt-8 sm:flex sm:items-center sm:justify-between abc">
+          <div>
           <p className="text-center text-xs text-gray-900">
             &copy; 2022
             {new Date().getFullYear() === 2022
@@ -242,6 +246,8 @@ const Footer = () => {
               : '- ' + new Date().getFullYear()}{' '}
             IEEE RVCE
           </p>
+          </div>
+          
           {/*<div className="flex flex-col items-center justify-center gap-4">
             <span className="text-center text-xs text-gray-500">
               Collaboration with{" "}
@@ -283,7 +289,8 @@ const Footer = () => {
           </div> */}
           <div>
             <p className="text-sm text-gray-500">
-              Developed by <br />
+              <Center>
+              Developed by </Center>
               <a
                 target="_blank"
                 className="font-bold text-indigo-500"
@@ -296,6 +303,40 @@ const Footer = () => {
           </div>
         </div>
       </div>
+      <style>
+        {
+        `
+          @media (max-width: 500px) {
+            footer {
+              padding: 10px;
+            }
+            .text-xs {
+              font-size: 12px; 
+            }
+            .mainDiv
+            {
+               display:flex;
+               align-items:center;
+               justify-content-center;
+               flex-direction:column;
+               padding:5px 5px;
+            }
+          }
+
+          @media (max-width: 1035px) {
+          .abc
+          {
+             display:flex;
+             flex-direction:column;
+             align-items:center;
+             justify-content-center;
+          }
+
+          }
+        `
+
+        }
+      </style>
     </footer>
   )
 }
