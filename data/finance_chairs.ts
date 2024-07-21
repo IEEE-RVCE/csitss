30e4b99adfb2fd59ebeaae40db44c675272a375f
Sundarakrishnan N <72200790+SundarakrishnanN@users.noreply.github.com>
2024-07-21 06:56:50 +0000

    fix finance

--- data/finance_chairs.ts
+++ data/finance_chairs.ts
@@ -5,6 +5,11 @@
     imgLink:'https://rvce.edu.in/sites/default/files/IMG-5954.jpg',
   },
   {
+    name:'Dr. Manjunatha Reddy A H',
+    post:'Associate Professor, Department of BT, RVCE',
+    imgLink:'http://www.rvce.edu.in/sites/default/files/AHM_2.jpg',
+  },
+  {
     name:'Mrs. Shyamala S',
     post:' Senior Manager-Finance, Accounts Section, RVCE',
     imgLink:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt_NZykul07nU3cliFuRZQr4_q-gOdkRTmRA&s',
@@ -14,7 +19,6 @@
     post: 'System Administrator',
     imgLink:'/ravi-varma.jpg',
   },
-
     
 ]
 export default finance_chairs_data;
