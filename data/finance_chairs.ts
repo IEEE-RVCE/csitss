cd948a2fcfead52abf42a1e99f8b406c8717ed98
Sundarakrishnan N <72200790+SundarakrishnanN@users.noreply.github.com>
2024-07-21 06:54:08 +0000

    fixed

---
+++ data/finance_chairs.ts
@@ -0,0 +1,24 @@
+ const finance_chairs_data = [
+  {
+    name:'Dr. Usha Rani K R',
+    post:'Professor, Department of ECE, RVCE',
+    imgLink:'https://rvce.edu.in/sites/default/files/IMG-5954.jpg',
+  },
+  {
+    name:'Dr. Manjunatha Reddy A H',
+    post:'Associate Professor, Department of BT, RVCE',
+    imgLink:'http://www.rvce.edu.in/sites/default/files/AHM_2.jpg',
+  },
+    {
+      name:'Mrs. Shyamala S',
+      post:' Senior Manager',
+      imgLink:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt_NZykul07nU3cliFuRZQr4_q-gOdkRTmRA&s',
+    },
+    {
+      name: ' Mr. Ravi Varma R B',
+      post: 'System Administrator',
+      imgLink:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt_NZykul07nU3cliFuRZQr4_q-gOdkRTmRA&s',
+    },
+    
+]
+export default finance_chairs_data;
