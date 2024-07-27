7caff007c500344eed118568f88140bff9483f13
Ayush <ayushratan082@gmail.com>
2024-07-27 19:49:57 +0530

    fix

--- pages/tracks.tsx
+++ pages/tracks.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 
 function Papers() {
   return (
-    <div className="mt-10 bg-gradient-to-b from-blue-50 to-blue-100 py-10">
-      <aside className="bg-white shadow-lg rounded-lg p-4 sm:p-8 lg:p-12 mx-auto max-w-4xl">
+    <div className="mt-10 bg-light-blue-100 py-10">
+      <aside className="bg-white shadow-lg rounded-lg p-6 sm:p-8 lg:p-12 mx-auto max-w-4xl">
         <div className="text-center">
-          <p className="text-2xl sm:text-3xl font-bold text-blue-700 sm:text-5xl">
-            Conference Tracks
-          </p>
+          <p className="text-3xl sm:text-5xl font-bold text-blue-700">Conference Tracks</p>
         </div>
         <p className="text-justify text-lg sm:text-2xl font-medium text-gray-700 my-4">
           Authors are invited to submit their original research work in the following areas (but not limited to):
         </p>
         <ul className="text-justify text-base sm:text-xl font-medium text-gray-600 list-none space-y-4">
-          <li className="mb-4">
+          <li>
             <b>Computational Analysis of Structural and Functional Materials:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Energy Materials, Nanomaterials, Smart Materials</li>
@@ -22,7 +20,7 @@ function Papers() {
               <li>Target Tracking/Recognition</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Green Electronics: Sustainable Practices:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Eco-Friendly Materials for Electronics</li>
@@ -31,7 +29,7 @@ function Papers() {
               <li>Biodegradable and Organic Electronics</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Robotics and Automation:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Artificial Intelligence in Robotics</li>
@@ -40,7 +38,7 @@ function Papers() {
               <li>Robotic Perception and Sensing</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Photonics, Optoelectronics and Mechatronics Systems:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Fiber optics and optical communications</li>
@@ -49,7 +47,7 @@ function Papers() {
               <li>Micro-electromechanical systems (MEMS)</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Artificial Intelligence for Sustainable World:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>AI and ML engineering and science applications</li>
@@ -60,7 +58,7 @@ function Papers() {
               <li>Generative AI for society and industry</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Digital Transformation in Various Sectors of Economy:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Digital transformation in Logistics</li>
@@ -69,7 +67,7 @@ function Papers() {
               <li>E-commerce and E-business</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Renewable Energy and Environmental Engineering:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Microgrids & Smart grids</li>
@@ -78,9 +76,10 @@ function Papers() {
               <li>Distributed Energy system and Control Strategies</li>
               <li>Power System operation, resilience, monitoring and control</li>
               <li>Carbon Capture, Utilization & Storage (CCUS) and Alternative Fuels</li>
+              <li>Sustainable construction materials and technologies</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Cyber-Physical Systems:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Next-Generation CPS Architectures</li>
@@ -89,7 +88,7 @@ function Papers() {
               <li>Ethical Considerations in CPS Design and Deployment</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Biomedical Electronics and Healthcare Applications:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Biomedical Image processing</li>
@@ -97,7 +96,7 @@ function Papers() {
               <li>Point of care devices, Bio_Mems</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Quantum Mechanics and Computing:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Hybrid quantum-classical computing systems</li>
@@ -105,7 +104,7 @@ function Papers() {
               <li>Quantum machine learning and optimization algorithms</li>
             </ul>
           </li>
-          <li className="mb-4">
+          <li>
             <b>Next-Generation Communication Systems:</b>
             <ul className="pl-6 list-disc list-inside space-y-2">
               <li>Internet of Things (IoT), 5G and Beyond</li>
@@ -118,7 +117,6 @@ function Papers() {
             </ul>
           </li>
         </ul>
-        
       </aside>
     </div>
   );
