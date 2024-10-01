31c62e8ac6348ac86fd4398d5873369b72eb44ee
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-10-01 18:15:46 +0530

    speakers card

--- components/SpeakerCard.tsx
+++ components/SpeakerCard.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
-const SpeakerCard = () => {
+type SpeakerType = {
+  id: number
+  name: string
+  designation: string
+  imageLink: string
+}
+
+const SpeakerCard = ({ speaker }: { speaker: SpeakerType }) => {
   return (
-    <div>
-      
+    <div
+      key={speaker.id}
+      className="flex w-72 flex-col space-y-2 rounded-2xl p-4 shadow-lg transition-all duration-300 hover:-translate-y-3"
+    >
+      <img
+        src={speaker.imageLink}
+        className="h-64 w-64 rounded-xl"
+        alt={speaker.name}
+      />
+      <div>
+        <h1 className="text-xl font-bold">{speaker.name}</h1>
+        <h1 className="text-lg text-gray-500">{speaker.designation}</h1>
+      </div>
     </div>
   )
 }
