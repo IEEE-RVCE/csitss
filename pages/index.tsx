81df674da35467e730d1244629c0c2f7b52a6d82
Ta-h-a <tahasindoli@gmail.com>
2025-05-08 13:30:22 +0530

    Removed double quotes

--- pages/index.tsx
+++ pages/index.tsx
@@ -248,7 +248,7 @@ export default function Index() {
                 was used for managing the peer-reviewing process for this
                 conference. This service was provided for free by Microsoft and
                 they bore all expenses, including costs for Azure cloud services
-                as well as for software development and support." Thanks and
+                as well as for software development and support. Thanks and
                 regards,
               </p>
 
