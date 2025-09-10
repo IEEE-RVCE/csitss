f1007f5b77f595ee9e2ec7bc47cc68d8db906fa4
SundarakrishnanN <sundar050804@gmail.com>
2025-09-10 22:42:51 +0530

    Revert "updatred keynote"
    This reverts commit 0c06b45a7e5547ed0cac31e7ebbcb911a72c525a.

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -12,20 +12,30 @@ type SpeakerType = {
 const keynoteSpeakers: SpeakerType[] = [
   {
     id: 1,
-    name: 'Prof. Dr. Jean Meyer',
+    name: 'Sri. S S Iyengar',
     designation:
-      'President of the Technical University of Applied Sciences Wuerzburg-Schweinfurt (THWS)',
-    topic:'Humanoid Robots – More than just another Industrial Revolution',
-    imageLink: './speakers/Jean.png',
+      'Distinguished University Professor, Florida International University (FIU), Miami',
+    imageLink: './speakers/ss_iyenger.jpeg',
   },
   {
     id: 2,
-    name: 'Prof. Dr. Achim Förster',
-    designation: 'Vice President for International & Academic Affairs at THWS',
-    topic:'AI Regulation under the EU AI Act',
-    imageLink: './speakers/Achim.png',
-  },
-  
+    name: "Prof. Dr. Stephan Weyers",
+    designation: "Vice-Rector for Teaching, Studies and International Affairs, FH Dortmund.",
+    imageLink: "/speakers/stephanWeyers.png",
+  }
+  // {
+  //   id: 2,
+  //   name: 'Chaitra Vedullapalli',
+  //   designation: 'Cofounder & CMO, Meylah',
+  //   imageLink: './speakers/chaitra_v.jpeg',
+  // },
+  // {
+  //   id: 3,
+  //   name: 'Prof. Dr.-Ing. Vinod Rajamani',
+  //   designation: 'Professor, University of Applied Sciences and Arts Dortmund',
+  //   imageLink:
+  //     'https://www.fh-dortmund.de/kontakt-daten/Rajamani-Vinod.php.media/82194/Vinod-Rajamani_Portrait.jpg.scaled/3312b7eed140ac54332cfc22f82c192c.jpg',
+  // },
 ]
 
 const aiForAllSpeakers: SpeakerType[] = [
@@ -89,15 +99,15 @@ const Speakers = () => {
           </div>
         ))}
       </div>
-{/* 
-      
+
+      {/* Pre Conference Speakers Section */}
       <div className="mt-16 flex justify-center">
         <h1 className="w-fit rounded-full bg-gradient-to-r from-teal-600 via-green-600 to-yellow-600 bg-clip-text px-6 py-3 text-4xl font-extrabold text-transparent shadow-lg">
           Invited Speakers
         </h1>
       </div>
 
-     
+      {/* AI for All Track */}
       <div className="mt-12 flex justify-center">
         <h2 className="w-fit rounded-full bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 bg-clip-text px-6 py-3 text-3xl font-extrabold text-transparent shadow-lg">
           Track 1: AI for All
@@ -114,7 +124,7 @@ const Speakers = () => {
         ))}
       </div>
 
-
+      {/* Digital Transformation Track */}
       <div className="mt-16 flex justify-center">
         <h2 className="w-fit rounded-full bg-gradient-to-r from-pink-600 via-red-600 to-yellow-600 bg-clip-text px-6 py-3 text-3xl font-extrabold text-transparent shadow-lg">
           Track 2: Digital Transformation
@@ -130,9 +140,7 @@ const Speakers = () => {
           </div>
         ))}
       </div>
-   */}
     </div>
-       
   )
 }
 
