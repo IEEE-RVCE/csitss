5c98def5a670a58ddfff548c5c062d3c2ac9fe71
Sathish Dath <sathishdutt0@gmail.com>
2025-10-31 16:17:03 +0530

    updated

--- components/TimeLine.tsx
+++ components/TimeLine.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Calendar, Clock, Download } from 'lucide-react'
+import { Calendar, Clock } from 'lucide-react'
 
 const CustomTimeline = () => {
   const [isVisible, setIsVisible] = useState(false)
