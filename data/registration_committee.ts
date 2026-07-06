8d9e228bd307f6471f807a82dca0dbc52aaad3c5
aryank25 <aryankumar251204@gmail.com>
2026-07-06 19:15:24 +0530

    updated remaining names

--- data/registration_committee.ts
+++ data/registration_committee.ts
@@ -20,16 +20,16 @@ const registration_committee_data = [
     post: 'Registration Committee',
     department: 'Electronics and Telecommunication Engineering',
     position: 'Assistant Professor',
-    imgLink: '',
-    email: '',
+    imgLink: 'https://rvce.edu.in/department/wp-content/uploads/2025/10/MITHUN-T-P-1-1229x1536.jpg',
+    email: 'mithuntp@rvce.edu.in',
   },
   {
     name: 'Dr. Shanthi P',
     post: 'Registration Committee',
     department: 'Electronics and Telecommunication Engineering',
     position: 'Associate Professor',
-    imgLink: '',
-    email: '',
+    imgLink: 'https://rvce.edu.in/department/wp-content/uploads/2025/07/SHANTHI-P-ETE-1-1229x1536.jpg',
+    email: 'shanthip@rvce.edu.in',
   },
   {
     name: 'Dr. Lingaya Hiremath',
@@ -38,6 +38,51 @@ const registration_committee_data = [
     position: 'Assistant Professor',
     imgLink: '',
     email: '',
+  },
+
+  {
+    name: 'Dr Usha Padma',
+    post: 'Registration Committee',
+    department: 'Electronics and Telecommunication Engineering',
+    position: 'Assistant Professor',
+    imgLink: 'https://rvce.edu.in/department/wp-content/uploads/2025/07/USHA-PADMA-1-1229x1536.jpg',
+    email: 'kamakshimb@rvce.edu.in',
+  },
+
+  {
+    name: 'Dr. Sunil S',
+    post: 'Registration Committee',
+    department: 'Civil Engineering',
+    position: 'Assistant Professor',
+    imgLink: '',
+    email: 'sunils@rvce.edu.in',
+  },
+
+  {
+    name: 'Dr. Anjaneya G',
+    post: 'Registration Committee',
+    department: 'Mechanical Engineering',
+    position: 'Assistant Professor',
+    imgLink: '',
+    email: 'anjaneyag@rvce.edu.in',
+  },
+
+  {
+    name: 'Prof. Ravi Kiran S Wali',
+    post: 'Registration Committee',
+    department: 'Civil Engineering',
+    position: 'Assistant Professor',
+    imgLink: 'https://rvce.edu.in/department/wp-content/uploads/2025/07/RAVIKIRAN-S-WALI-1-1229x1536.jpg',
+    email: 'ravikiransw@rvce.edu.in',
+  },
+
+  {
+    name: 'Dr. Prapul Chandra',
+    post: 'Registration Committee',
+    department: 'Mechanical Engineering',
+    position: 'Assistant Professor',
+    imgLink: '',
+    email: 'prapulchandra@rvce.edu.in',
   },
 ]
 
