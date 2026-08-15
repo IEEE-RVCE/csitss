a5536c1f8daa6709a02454690d292178e45ffcb8
aryank25 <aryankumar251204@gmail.com>
2026-08-15 17:47:17 +0530

    updated finance

--- data/finance_committee.ts
+++ data/finance_committee.ts
@@ -4,7 +4,7 @@ const finance_chairs_data = [
     post: 'Finance Chair',
     department: '',
     position: 'Principal',
-    imgLink: '',
+    imgLink: 'https://rvce.edu.in/wp-content/uploads/2025/09/Group-12861-2.png',
     email: 'subramanyakn@rvce.edu.in',
   },
   {
@@ -12,7 +12,7 @@ const finance_chairs_data = [
     post: 'Finance Committee',
     department: '',
     position: 'Vice Principal',
-    imgLink: '',
+    imgLink: 'https://rvce.edu.in/department/wp-content/uploads/2025/07/Vice-Principal-1-1229x1536.jpg',
     email: 'geethaks@rvce.edu.in',
   },
   {
@@ -20,7 +20,7 @@ const finance_chairs_data = [
     post: 'Finance Committee',
     department: 'Biotechnology',
     position: 'Assistant Professor',
-    imgLink: '',
+    imgLink: 'https://media.licdn.com/dms/image/v2/C5103AQGV8bupyCIOQw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1530028779550?e=1788393600&v=beta&t=6sBC8zY3C--FkhgZ4IZN7jF5oWJP5iO1L6rQOwJbZn8',
     email: 'trilokc@rvce.edu.in',
   },
   {
